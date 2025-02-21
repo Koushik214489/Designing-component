cfde84a03971a91a6c91420f60451f499b4bebc5
Your Name <your-email@example.com>
2025-02-21 08:56:57 +0530

    Completed product

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,9 @@
-// write button card here
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button>View Product</button>
+  );
+};
+
+export default Button;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,19 @@
-// write product card here
+import React from 'react';
+import Button from '../components/button';
+
+const Productcard = () => {
+  const productImage = 'https://picsum.photos/200/300';
+  const productName = 'Product Name';
+  const productPrice = '$100';
+
+  return (
+    <div className="card">
+      <img src={productImage} alt={productName} />
+      <h2>{productName}</h2>
+      <p>Price: {productPrice}</p>
+      <Button />
+    </div>
+  );
+};
+
+export default Productcard;
